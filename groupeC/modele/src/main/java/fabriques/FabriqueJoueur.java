7ddf1f2348e6ca255238d2441e4d894df616a26f
Agboton Kilian <kilian.agboton@etu.univ-orleans.fr>
2017-04-10 14:38:13 +0200

    Creation de FabriqueJoueur

--- groupeC/modele/src/main/java/fabriques/FabriqueJoueur.java
+++ groupeC/modele/src/main/java/fabriques/FabriqueJoueur.java
@@ -1,7 +1,27 @@
 package fabriques;
 
+import joueurs.Joueur;
+
 /**
  * Created by David on 10/04/2017.
  */
 public class FabriqueJoueur {
+
+    private static FabriqueJoueur instance;
+
+    private FabriqueJoueur(){
+
+    }
+
+    public static FabriqueJoueur getInstance(){
+        if (instance == null){
+            instance = new FabriqueJoueur();
+        }
+
+        return instance;
+    }
+
+    public Joueur creerJoueur(String pseudo, String password){
+        return new Joueur(pseudo, password);
+    }
 }
