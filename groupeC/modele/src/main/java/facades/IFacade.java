53029bdc9d9b70e4059c7e6d4c192a1c496aa8fb
idir.amrouche <idiramrouche7@gmail.com>
2017-04-10 15:00:14 +0200

    Création de l'interface IFacade

--- groupeC/modele/src/main/java/facades/IFacade.java
+++ groupeC/modele/src/main/java/facades/IFacade.java
@@ -1,7 +1,33 @@
 package facades;
 
+import exceptions.IdentifiantsIncorrectsException;
+import exceptions.JoueurDejaConnecteException;
+import exceptions.PseudoDejaExistantException;
+
 /**
  * Created by David on 10/04/2017.
  */
 public interface IFacade {
+
+
+    /* Gestion des utilisateurs */
+    void inscription(String pseudo, String password) throws PseudoDejaExistantException;
+    void connexion(String pseudo, String password) throws JoueurDejaConnecteException, IdentifiantsIncorrectsException;
+    void deconnexion(String pseudo);
+    boolean estInscrit(String pseudo);
+    boolean estConnecte(String pseudo);
+
+
+    /* Gestion des salons */
+    void creerSalon(String pseudo);
+    void rejoindreSalon(String pseudo, int idSalon);
+
+    /* Préparation de la partie */
+    void intervertirFantomes(int idSalon, String pseudo, int xSource, int ySource, int xDestination, int yDestination);
+    void validerPlacementFantomes(int idSalon, String pseudo);
+
+    /* Gestion de la partie */
+    void effectuerAction(int idSalon, String pseudo, int xSource, int ySource, int xDestination, int yDestination);
+    void quitterPartie(int idSalon, String pseudo);
+    void quitterSalon(int idSalon, String pseudo);
 }
