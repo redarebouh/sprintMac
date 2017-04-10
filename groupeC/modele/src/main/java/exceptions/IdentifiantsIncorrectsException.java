c83731387e58af5ff61978464134588144080066
idir.amrouche <idiramrouche7@gmail.com>
2017-04-10 14:59:34 +0200

    Ajout des exceptions pour l'inscription et la connexion

---
+++ groupeC/modele/src/main/java/exceptions/IdentifiantsIncorrectsException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+/**
+ * Created by alphalion27 on 10/04/17.
+ */
+public class IdentifiantsIncorrectsException extends Exception {
+    public IdentifiantsIncorrectsException(String s){
+        super(s);
+    }
+}
