dae639e2e3b7b8728158eeae42bf728419eb49f2
Maaroufi Khaled <khaled.maaroufi@etu.unv-orleans.fr>
2017-04-10 14:21:01 +0200

    creation de la classe Joueur

--- groupeC/modele/src/main/java/joueurs/Joueur.java
+++ groupeC/modele/src/main/java/joueurs/Joueur.java
@@ -4,4 +4,42 @@ package joueurs;
  * Created by David on 10/04/2017.
  */
 public class Joueur {
+
+
+    private int id;
+    private String pseudo;
+    private String password;
+
+    private static int identifiant=0;
+
+    public Joueur(String nom,String mdp){
+        this.id = identifiant;
+        this.pseudo = nom;
+        this.password = mdp;
+        identifiant++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
